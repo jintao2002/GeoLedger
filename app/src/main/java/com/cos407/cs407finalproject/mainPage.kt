3b312ce09956d5dc69c3c271b2d9aa47ca7e13e5
guo343 <guo343@wisc.edu>
2024-09-30 01:51:43 -0500

    create a new page

--- app/src/main/java/com/cos407/cs407finalproject/mainPage.kt
+++ app/src/main/java/com/cos407/cs407finalproject/mainPage.kt
@@ -1,28 +1,20 @@
 package com.cos407.cs407finalproject
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class mainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        findViewById<Button>(R.id.goToMainPageButton).setOnClickListener {
-            val intent = Intent(this, mainPage::class.java)
-            startActivity(intent)
-        }
-
     }
 }
