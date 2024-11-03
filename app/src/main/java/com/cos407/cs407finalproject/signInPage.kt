25b93f5763bce186a10878da9a65c402f08381a8
guo343 <guo343@wisc.edu>
2024-11-03 00:47:11 -0500

    home page, sign in page, sign up page

---
+++ app/src/main/java/com/cos407/cs407finalproject/signInPage.kt
@@ -0,0 +1,36 @@
+package com.cos407.cs407finalproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class signInPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_in_page)
+
+        setContentView(R.layout.activity_sign_in_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<ImageButton>(R.id.btnBack).setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.page_menu, menu)
+        return true
+    }
+
+}
