7d8c3904716eb6d630d650851a5bd3dc4675b12f
JQin-UWM <jqin38@wisc.edu>
2024-11-04 10:19:29 -0600

    Update On Logout Btn

--- app/src/main/java/com/cos407/cs407finalproject/recordPage.kt
+++ app/src/main/java/com/cos407/cs407finalproject/recordPage.kt
@@ -1,6 +1,8 @@
 package com.cos407.cs407finalproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class recordPage : AppCompatActivity() {
@@ -8,5 +10,13 @@ class recordPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_page)
+
+        // Find the Logout button and set up a click listener
+        findViewById<Button>(R.id.btnLogout).setOnClickListener {
+            // Create an Intent to navigate back to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close the recordPage
+        }
     }
 }
