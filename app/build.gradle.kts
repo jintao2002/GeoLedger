62b7e1e7b7cd22070ccdc190d04926637f2ec754
baite123 <baiyahahha9@gmail.com>
2024-11-04 20:17:50 -0600

    Welcome page implemented. Deleted "Me" for UI and further implementation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.airbnb.android:lottie:6.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
